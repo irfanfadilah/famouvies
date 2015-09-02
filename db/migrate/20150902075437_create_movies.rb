41bc7fe3421a7bf706da0ccdbc38b8b600687be2
Irfan Fadilah <irfan@41studio.com>
2015-09-02 15:02:52 +0700

    add movie model and seed data

---
+++ db/migrate/20150902075437_create_movies.rb
@@ -0,0 +1,18 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.integer :tmdb
+      t.string :title
+      t.text :overview
+      t.date :release_date
+      t.string :poster
+      t.string :backdrop
+      t.string :language
+      t.float :popularity
+      t.float :rating
+      t.integer :vote
+
+      t.timestamps null: false
+    end
+  end
+end
